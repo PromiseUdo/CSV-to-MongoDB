66b83142014374baeb55cbf8cd3d6e991c9b64db
Promise Udo <info.promiseudo@gmail.com>
2021-03-17 19:05:45 +0100

    csv upload and parse first commit

---
+++ app.js
@@ -0,0 +1,81 @@
+const express = require('express');
+const csv = require('csv-parser');
+const fs = require('fs');
+const multer = require('multer');
+const mongoose = require('mongoose');
+const Candidate = require('./models/candidate');
+
+const results = [];
+const app = express();
+
+//connection to the database
+mongoose.connect('mongodb://localhost:27017/csvproject', {
+        useNewUrlParser: true, 
+        useCreateIndex:true,
+        useUnifiedTopology: true,
+        useFindAndModify:false
+    });
+
+const db = mongoose.connection; //bind mongoose.connection to db
+
+db.on('error', console.error.bind(console,'Connection Error:'));
+db.once('open', ()=>{
+    console.log('Database Connected!');
+});
+
+//create the file storage engine using multer
+const fileStorageEngine = multer.diskStorage({
+    destination:(req, file, cb)=>{
+        cb(null, './csv' )
+    },
+    filename:(req, file, cb)=>{
+        cb(null, file.originalname)
+    }
+});
+
+const upload = multer({
+    storage:fileStorageEngine
+});
+
+//checking if the file exists
+
+const path = './csv/data.csv'
+
+
+app.post('/single', upload.single('candidates'),  async (req, res)=>{
+    // console.log(req.file);
+    res.send("CSV file successfull");
+try {
+    //check if the file exists before saving to DB
+    if (fs.existsSync(path)){
+      //file exists
+      console.log('File exists');
+      fs.createReadStream('./csv/data.csv')
+      .pipe(csv({}))
+      .on('data', (data)=>results.push(data))
+      .on('end',async()=>{
+          console.log(results);
+        for(let person of results){
+            //generate a random password for each candidate
+            const password = ticketId => Math.floor(Math.random() * 999999) + 10000;
+            //pass along side the data form Excel the password for each candidate
+            const newCandidate = new Candidate({...person, password:`LOC${password()}`});
+            //save the candidate to the database
+            await newCandidate.save();
+
+      }});
+    }else{
+        console.log("File does not exist");
+    }
+  } catch(err) {
+    console.error(err)
+  }
+})
+
+app.get("/", (req, res)=>{
+    res.sendFile(__dirname + "/index.html");
+})
+
+app.listen(3000, ()=>{
+    console.log("App started on port 3000");
+})
---
+++ models/candidate.js
@@ -0,0 +1,31 @@
+const mongoose = require('mongoose');
+// const passportLocalMongoose = require('passport-local-mongoose');
+const Schema = mongoose.Schema;
+
+
+//create a staff schema
+const candidateSchema = new Schema({
+    username:{
+        type:String
+    },
+    phone: {
+        type:String,
+        required:true,
+        unique:true
+    },
+    email:{
+        type:String
+    },
+    course:{
+        type:String
+    },
+    password:{
+        type:String
+    }
+});
+
+//will add on fields for username and password, makes them unique and give us additional methods to use
+
+
+//compile and export
+module.exports = mongoose.model('Candidate', candidateSchema);
